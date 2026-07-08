d1618dbe51602a54b62b8d353b77e4efe7fc0022
Faiaz-Ali <faizaali.666555@gmail.com>
2026-07-08 11:21:27 +0500

    notification migration file created successfully

--- database/migrations/2026_07_08_062021_create_notifications_table.php
+++ database/migrations/2026_07_08_062021_create_notifications_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('visitors', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('visitors');
+        Schema::dropIfExists('notifications');
     }
 };
