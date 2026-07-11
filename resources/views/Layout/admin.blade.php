1a9a929f605ae0838b02de4fb409e64b45e1b07f
Zainab-Eman-Butt <zainabemanbutt@gmail.com>
2026-07-11 11:25:32 +0500

    2 link add in Layout.admin file for loading the icons for Admin panel pages

--- resources/views/Layout/admin.blade.php
+++ resources/views/Layout/admin.blade.php
@@ -8,7 +8,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
         :root {
             --sidebar-width: 260px;
